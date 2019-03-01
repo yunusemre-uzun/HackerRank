d290c9c62bfae749c9ee9c6142a2812541412fac
yunusemre-uzun <yunus.uzun@metu.edu.tr>
2019-03-01 23:51:36 +0300

    Two more solution are shared. easy/q013 and easy/q014

---
+++ easy/q013.cpp
@@ -0,0 +1,95 @@
+// Problem name : Sock Merchant
+// Problem URL : https://www.hackerrank.com/challenges/sock-merchant/problem
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the sockMerchant function below.
+int sockMerchant(int n, vector<int> ar) {
+    vector<int> waitingList;
+    size_t i,j;
+    unsigned int waitingListsize = 0;
+    unsigned int pairNumber = 0;
+    bool flag;
+    for(i=0;i<n;i++){
+        flag = false;
+        for(j=0;j<waitingListsize;j++){
+            if(ar[i] == waitingList[j]){
+                pairNumber++;
+                waitingList[j] = -1;
+                flag = true;
+                break;
+            }
+        }
+        if(flag)
+            continue;
+        waitingList.push_back(ar[i]);
+        waitingListsize++;   
+    }
+    return pairNumber;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string ar_temp_temp;
+    getline(cin, ar_temp_temp);
+
+    vector<string> ar_temp = split_string(ar_temp_temp);
+
+    vector<int> ar(n);
+
+    for (int i = 0; i < n; i++) {
+        int ar_item = stoi(ar_temp[i]);
+
+        ar[i] = ar_item;
+    }
+
+    int result = sockMerchant(n, ar);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
